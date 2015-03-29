3d53028b0f415c9d94da650549de5ce840463e22
christopher lee <clee@edx.org>
2015-03-29 13:16:12 -0400

    blog model

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+import datetime
 
 
-class Blog(models.Model):
-    """Model la Blog"""
-    title = models.CharField(max_length=255)
-    publish_date = models.DateTimeField('date published')
-
-
+class Post(models.Model):
+    """Post model."""
+    title = models.CharField(max_length=200)
+    # author
+    body = models.TextField()
+    publish_date = models.DateTimeField(default=datetime.datetime.now)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
--- chartartar/urls.py
+++ chartartar/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'chartartar.views.home', name='home'),
